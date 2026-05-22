8f8df103bdfd2fe7c804d42b9358a2bf6fe37a68
del Amo Calle <laura.del-amo-calle@capgemini.com>
2026-05-22 10:39:45 +0200

    Gateway configurado

---
+++ server-springboot-micros/server-springboot-gateway/tutorialgateway/src/test/java/com/ludoteca/tutorialgateway/TutorialgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ludoteca.tutorialgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
